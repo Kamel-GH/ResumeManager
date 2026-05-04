5a42c767004eeb7ff0b83d6ad4a59b582fbc958d
Kamel-GH <kamel@mebarki.net>
2026-05-04 20:46:11 +0200

    refactor(editor): export editor view selectors

---
+++ features/editor/selectors/index.ts
@@ -0,0 +1,3 @@
+export * from "@/features/editor/selectors/editor-pages-view";
+export * from "@/features/editor/selectors/editor-layers-view";
+export * from "@/features/editor/selectors/editor-objects-view";
